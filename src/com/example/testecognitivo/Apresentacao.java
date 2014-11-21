5db2c7aa5c45b45a90e05cb6604dd4bff71cc904
liebersonsantos <liebersonsantos.one@gmail.com>
2014-11-21 21:32:32 -0200

    implementacao de classes - cadastromed e apresentacao - lieberson

--- src/com/example/testecognitivo/Apresentacao.java
+++ src/com/example/testecognitivo/Apresentacao.java
@@ -1,17 +1,34 @@
 package com.example.testecognitivo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Apresentacao extends Activity{
-	
+public class Apresentacao extends Activity {
+
+	Button btnInicioTeste;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.apresentacao);
-		
+
+		btnInicioTeste.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				Intent intent = new Intent(Apresentacao.this,
+						OrientacaoTemporalActivity.class);
+				startActivity(intent);
+
+			}
+		});
+
 	}
 
 }
--- src/com/example/testecognitivo/CadastroMedicoActivity.java
+++ src/com/example/testecognitivo/CadastroMedicoActivity.java
@@ -1,18 +1,66 @@
 package com.example.testecognitivo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class CadastroMedicoActivity extends Activity{
-	
+public class CadastroMedicoActivity extends Activity {
+
+	/**
+	 * @author lieberson Declaracao de variaveis
+	 */
+
+	EditText edtNomeMed, edtCrmMed, edtEspecialidadeMed, edtLoginMed,
+			edtSenhaMed;
+	Button btnSalvarMed;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.cadastro_medico);
-	//comentario
-	
+
+		/**
+		 * Linka as variaveis do java com o xml ativando o botao salvar
+		 * */
+
+		edtNomeMed = (EditText) findViewById(R.id.edtNomeMed);
+		edtCrmMed = (EditText) findViewById(R.id.edtCrmMed);
+		edtEspecialidadeMed = (EditText) findViewById(R.id.edtEspecialidadeMed);
+		edtLoginMed = (EditText) findViewById(R.id.edtLoginMed);
+		edtSenhaMed = (EditText) findViewById(R.id.edtSenhaMed);
+
+		/**
+		 * @author lieberson 
+		 * captura o evento de clique do botao
+		 */
+
+		btnSalvarMed.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				/**
+				 * tem que ser implementado um metodo para gravar os dados no
+				 * banco. apos isso, sera redirecionado para a tela de login
+				 */
+
+				
+				/**
+				 * criando o objeto intent para redirecionamento para a tela de
+				 * login
+				 */
+				Intent intent = new Intent(CadastroMedicoActivity.this,
+						LoginActivity.class);
+
+				startActivity(intent);
+
+			}
+		});
+
 	}
 
 }
