4d96837a9aec17798c4abf284ea4811f65834e4b
liebersonsantos <liebersonsantos.one@gmail.com>
2014-11-21 19:10:20 -0200

    criacao layout registro - lieberson

---
+++ src/com/example/testecognitivo/RegistroActivity.java
@@ -0,0 +1,16 @@
+package com.example.testecognitivo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistroActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.registro);
+	}
+
+}
