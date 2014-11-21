806cec6422e1f74f45a85785a641b80327b9b13d
liebersonsantos <liebersonsantos.one@gmail.com>
2014-11-21 19:59:20 -0200

    criacao layout e classe lembrancas - lieberson

---
+++ src/com/example/testecognitivo/LembrancasActivity.java
@@ -0,0 +1,17 @@
+package com.example.testecognitivo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LembrancasActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.lembrancas);
+		
+	}
+
+}
