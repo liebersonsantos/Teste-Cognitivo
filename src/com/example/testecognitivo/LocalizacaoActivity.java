cd4a6da4424d3440557beabb342448563e57c5d7
tairoroberto <github@nowsolucoes.com.br>
2014-11-21 20:10:06 -0200

    icones de botões - Tairo

---
+++ src/com/example/testecognitivo/LocalizacaoActivity.java
@@ -0,0 +1,13 @@
+package com.example.testecognitivo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocalizacaoActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.localizacao);
+	}
+}
