ae20fc5f72638ac9d248b88c81bea47f72c4afb2
tairoroberto <github@nowsolucoes.com.br>
2014-11-21 17:47:52 -0200

    tela dados do paciente - Tairo

---
+++ src/com/example/testecognitivo/DadosPacienteActivity.java
@@ -0,0 +1,13 @@
+package com.example.testecognitivo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DadosPacienteActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dados_paciente);
+	}
+}
