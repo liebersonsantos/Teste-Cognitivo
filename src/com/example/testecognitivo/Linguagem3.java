5abdd2ead3bc32797f487e4d1dbf792e91a5d79e
liebersonsantos <liebersonsantos.one@gmail.com>
2014-11-22 15:24:54 -0200

    criacao layout e classe linguagem3-inicio linguagem4  - lieberson

--- src/com/example/testecognitivo/Linguagem3.java
+++ src/com/example/testecognitivo/Linguagem3.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class Linguagem3 extends Activity {
+	
+	private ImageButton imgbtnSeguirLinguagem3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,20 @@ public class Linguagem3 extends Activity {
 		
 		// inflar layout de linguagem 3
 		setContentView(R.layout.linguagem);
+		
+		imgbtnSeguirLinguagem3 = (ImageButton) findViewById(R.id.imgbtnSeguirLinguagem3);
+		imgbtnSeguirLinguagem3.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Intent intent = new Intent(Linguagem3.this, Linguagem4.class);
+				startActivity(intent);
+				
+			}
+		});
+		
 	}
 
 }
---
+++ src/com/example/testecognitivo/Linguagem4.java
@@ -0,0 +1,19 @@
+package com.example.testecognitivo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Linguagem4 extends Activity{
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	setContentView(R.layout.linguagem4;
+	
+
+}
