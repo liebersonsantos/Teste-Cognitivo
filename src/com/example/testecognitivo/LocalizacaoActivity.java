bff4d2486cfb5641d53d965a2880773258dc4eac
lieberson <liebersonsantos.one@gmail.com>
2014-12-03 16:44:25 -0200

    mudanca no layout localizacao + cogigo + codigo para soma

--- src/com/example/testecognitivo/LocalizacaoActivity.java
+++ src/com/example/testecognitivo/LocalizacaoActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class LocalizacaoActivity extends Activity{
 	
 	//Declaração das variáveis
 	private Button btnProxiamPgLocalizacao;	
+	private ToggleButton tgLocal, tgInstituicao, tgBairro, tgCidade, tgEstado;
 	private int pontos;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -24,17 +29,73 @@ public class LocalizacaoActivity extends Activity{
 		
 		//passa os pontos da outra activity para a variável "ponto"
 		//para poder adicionar os pontos dessa Activity
-		pontos = bundlePontos.getInt("postos");
-		
-		
-		//Linka o Java com Xml
-		
+		pontos = bundlePontos.getInt("pontos");
+				
+		//Linka o Java com Xml		
 		//Aqui precisa pegar todos os botões do xml para implementar o clique
 		btnProxiamPgLocalizacao = (Button) findViewById(R.id.btnProxiamPgLocalizacao);
 		
-		//quando clicar no botão de V adicionar pamis um ponto na varial "ponto"
-		// ex: pontos++; "igual na tela anterior"
+		tgLocal = (ToggleButton) findViewById(R.id.tgLocal);
+		tgLocal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked){
+					pontos = pontos +1; Toast.makeText(LocalizacaoActivity.this, String.valueOf(pontos), Toast.LENGTH_SHORT).show();
+				}
+				
+			}
+		});
+				
+		tgInstituicao = (ToggleButton) findViewById(R.id.tgInstituicao);
+		tgInstituicao.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked){
+					pontos = pontos +1; Toast.makeText(LocalizacaoActivity.this, String.valueOf(pontos), Toast.LENGTH_SHORT).show();
+				}
+				
+			}
+		});
 		
+		tgBairro = (ToggleButton) findViewById(R.id.tgBairro);
+		tgBairro.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked){
+					pontos = pontos +1; Toast.makeText(LocalizacaoActivity.this, String.valueOf(pontos), Toast.LENGTH_SHORT).show();
+				}
+				
+			}
+		});
+		
+		tgCidade = (ToggleButton) findViewById(R.id.tgCidade);
+		tgCidade.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked){
+					pontos = pontos +1; Toast.makeText(LocalizacaoActivity.this, String.valueOf(pontos), Toast.LENGTH_SHORT).show();
+				}
+				
+			}
+		});
+			
+		tgEstado = (ToggleButton) findViewById(R.id.tgEstado);
+		tgEstado.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked){
+					pontos = pontos +1; Toast.makeText(LocalizacaoActivity.this, String.valueOf(pontos), Toast.LENGTH_SHORT).show();
+				}
+				
+			}
+		});
+		
+				
 		//evento de clique no botão
 		btnProxiamPgLocalizacao.setOnClickListener(new View.OnClickListener() {			
 			@Override
