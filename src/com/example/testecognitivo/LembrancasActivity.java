561a6675a4dc5a6918310186f57d1d95043f4627
lieberson <liebersonsantos.one@gmail.com>
2014-12-04 11:48:28 -0200

    mudancas no layout lembrancas + alteracoes na classe

--- src/com/example/testecognitivo/LembrancasActivity.java
+++ src/com/example/testecognitivo/LembrancasActivity.java
@@ -4,34 +4,103 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class LembrancasActivity extends Activity {
 	/**
-	 * @author tairo declaração de variaveis
+	 * @author lieberson
+	 *  declaração de variaveis
 	 */
-	ImageButton imgBtnEnviarLembrancas;
+	private ImageButton imgBtnEnviarLembrancas;
+	private ToggleButton tgVasoLem, tgCarroLem, tgTijoloLem;
+	private int pontos;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.lembrancas);
 
+		/*
+		 * Pega a pontuação da Activity anterior
+		 */
+		Intent intentPontos = getIntent();
+		/*
+		 * Bundle para pegar os dados da intent
+		 */
+		Bundle bundlePontos = intentPontos.getExtras();
+		/*
+		 * passa os pontos da outra activity para a variável "ponto"
+		 * para poder adicionar os pontos dessa Activity
+		 */
+		pontos = bundlePontos.getInt("pontos");		
+		
 		/**
 		 * Linka as variaveis java com xml
 		 */
 		imgBtnEnviarLembrancas = (ImageButton) findViewById(R.id.imgBtnEnviarLembrancas);
+		
+		tgVasoLem = (ToggleButton) findViewById(R.id.tgVasoLem);
+		tgVasoLem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked){
+					pontos = pontos +1;Toast.makeText(LembrancasActivity.this, String.valueOf(pontos), Toast.LENGTH_SHORT).show();
+					tgVasoLem.setEnabled(false);  
+				}
+				
+			}
+		});
+		
+		tgCarroLem = (ToggleButton) findViewById(R.id.tgCarroLem);
+		tgCarroLem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked){
+					pontos = pontos +1;Toast.makeText(LembrancasActivity.this, String.valueOf(pontos), Toast.LENGTH_SHORT).show();
+					tgCarroLem.setEnabled(false);
+				}
+				
+			}
+		});
+		
+		tgTijoloLem = (ToggleButton) findViewById(R.id.tgTijoloLem);
+		tgTijoloLem.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked){
+					pontos = pontos +1;Toast.makeText(LembrancasActivity.this, String.valueOf(pontos), Toast.LENGTH_SHORT).show();
+					tgTijoloLem.setEnabled(false);
+				}
+				
+			}
+		});	
+		
 		imgBtnEnviarLembrancas.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				/**
-				 * @author tairo envia para a tela de liguagem
+				 * @author lieberson
+				 * envia para a tela de liguagem
 				 */
 				Intent intent = new Intent(LembrancasActivity.this,Linguagem.class);
 				startActivity(intent);
+				
+				/*
+				 * Envia os valores para a proxima classe
+				 * 
+				 */
+				intent.putExtra("pontos", pontos);
+				startActivity(intent);
 			}
-		});
+		}); 
 
 	}
 
